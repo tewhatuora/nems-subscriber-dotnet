0d782bfab1bd5cda81df84eced9b2608ce292f60
James Taylor <james.taylor@sparkhealth.nz>
2024-04-09 12:40:35 +1200

    Added Test Unit

--- GuaranteedSubscriberTest/SubscriberTest.cs
+++ GuaranteedSubscriberTest/SubscriberTest.cs
@@ -25,6 +25,7 @@ namespace GuaranteedSubscriberTest
 
             mockDestination.Setup(d => d.Name).Returns("foo/bah");
             mockUserPropertyMap.Setup(p => p.GetString("source")).Returns("hip");
+            mockUserPropertyMap.Setup(p => p.GetString("id")).Returns("1234");
 
             // Set up behavior for the GetValue method of the mock
 
@@ -35,6 +36,7 @@ namespace GuaranteedSubscriberTest
 
             Assert.AreEqual("foo/bah", mockMessage.Object.Destination.Name);
             Assert.AreEqual("hip", mockMessage.Object.UserPropertyMap.GetString("source"));
+            Assert.AreEqual("1234", mockMessage.Object.UserPropertyMap.GetString("id"));
             Assert.AreEqual("Hello, NEMS!", Encoding.ASCII.GetString(mockMessage.Object.BinaryAttachment));
 
         }
@@ -46,12 +48,17 @@ namespace GuaranteedSubscriberTest
             var mockDestination = new Mock<IDestination>();
             var mockUserPropertyMap = new Mock<IMapContainer>();
             var mockMessage = new Mock<IMessage>();
-            byte[] mockPayloadBytes = Encoding.UTF8.GetBytes("Hello, NEMS!");
+            byte[] mockPayloadBytes = Encoding.UTF8.GetBytes("{\"callbackUrl\": \"https://api.hip-uat.digital.health.nz/fhir/nhi/v1/Patient/ZAT2348\", \"deathDate\": \"2016-02-18\"}");
             sbyte[] mockPayload = Array.ConvertAll(mockPayloadBytes, b => (sbyte)b);
 
 
-            mockDestination.Setup(d => d.Name).Returns("foo/bah");
-            mockUserPropertyMap.Setup(p => p.GetString("source")).Returns("hip");
+            mockDestination.Setup(d => d.Name).Returns("demographics/patient/death/new/0.1.0/G00036-D/2203/FZZ988-H/ZAT2348");
+            mockUserPropertyMap.Setup(p => p.GetString("source")).Returns("https://hip.uat.digital.health.nz");
+            mockUserPropertyMap.Setup(p => p.GetString("id")).Returns("86152393-fc5b-4b21-a315-5992f31d2aa1");
+            mockUserPropertyMap.Setup(p => p.GetString("datacontenttype")).Returns("application/json");
+            mockUserPropertyMap.Setup(p => p.GetString("time")).Returns("2024-04-08T04:18:47.11Z");
+            mockUserPropertyMap.Setup(p => p.GetString("type")).Returns("demographics/patient/death/new/0.1.0");
+            mockUserPropertyMap.Setup(p => p.GetString("subject")).Returns("ZAT2348");
 
             // Set up behavior for the GetValue method of the mock
 
@@ -59,8 +66,8 @@ namespace GuaranteedSubscriberTest
             mockMessage.Setup(m => m.UserPropertyMap).Returns(mockUserPropertyMap.Object);
             mockMessage.Setup(m => m.BinaryAttachment).Returns(mockPayloadBytes);
 
-            Console.WriteLine("This is a test message.");
             new EventLoader().ProcessEvent(mockMessage.Object);
+
         }
     }
 }
